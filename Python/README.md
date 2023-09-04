42e9faf432fac8455258cffe6eb4f8e73293a540
dlawle <daniel.lawler@outlook.com>
2023-09-03 21:31:21 -0400

    Adding python scripts for testing

---
+++ Python/README.md
@@ -0,0 +1,12 @@
+### NumPy SciPy Testing 
+# Overview
+`fft-testing.py` serves as a "template" for simple signal > FFT > iFFT > output. The script will create a wav file as well as graph both the preprocessed and postprocessed waveforms. Any additional scripts function will be noted by name. 
+
+# Required libs
+This script was written with Python3 v3.11.4. Libraries used are (will be updated):
+    - NumPy v1.24.2
+    - SciPy v1.11.1
+    - MatPlotLib v3.7.2
+
+# Full Disclosure
+I am a hobbyist with no formal training in this subject. While I may make assumptions/conclusions, it is not guaranteed to be accurate or correct. With that being said, I'm always up for feedback and comments! 
---
+++ Python/fft-testing.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.fft import fft, ifft
+from scipy.signal import hann
+from scipy.io import wavfile
+
+# Parameters
+sample_rate = 44100  # Sample rate in Hz
+duration = 1.0       # Duration in seconds
+frequency = 10.0    # Frequency in Hz
+
+# Generate the time values
+t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
+
+# Create a 440Hz sine wave
+sine_wave = np.sin(2 * np.pi * frequency * t)
+
+# Apply Hanning window
+window = hann(len(sine_wave))
+sine_wave *= window  # Apply the window to the sine wave
+
+# Perform FFT
+fft_result = fft(sine_wave)
+
+""" 
+    Notes:
+
+    Accessing FFT bins:
+    fft_result is an array containing complex values representing the frequency domain.
+    To access the magnitude and phase of each bin, you can use the following:
+    
+        magnitude = np.abs(fft_result)  # Magnitude of each bin
+        phase = np.angle(fft_result)   # Phase of each bin
+
+    You can then access individual bins by indexing fft_result like fft_result[index].
+    Manipulate the FFT result
+    Suggestions for manipulation/effects:
+        - Frequency Shifting
+        - Magnitude Scaling
+        - Phase Manipulation
+        - Filtering
+        - Noise Addition
+        - Time-Stretching/Compression
+        - Harmonic Generation
+
+    Example: Scale the magnitude of all bins by a factor of 0.5
+    fft_result = fft_result * 0.5
+"""
+
+
+# Perform IFFT
+ifft_result = ifft(fft_result)
+
+# Plot the original wave
+plt.subplot(2, 1, 1)
+plt.plot(t, sine_wave)
+plt.title("Original 440Hz Sine Wave with Hanning Window")
+
+# Plot the manipulated IFFT result
+plt.subplot(2, 1, 2)
+plt.plot(t, ifft_result)
+plt.title("Manipulated Reconstructed Wave (IFFT)")
+
+plt.tight_layout()
+plt.show()
+
+# Save the manipulated waveform as a WAV file
+output_filename = "output.wav"
+# Scale the waveform back to the original amplitude range [−32768, 32767]
+scaled_waveform = np.int16(ifft_result.real * 32767)
+wavfile.write(output_filename, sample_rate, scaled_waveform)
